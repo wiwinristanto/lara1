568e4b697308e909b660ce45b4f9b65802691011
wiwinristanto <wiwinristanto94@gmail.com>
2022-04-06 05:01:48 +0700

    menambah fitur controller post dan model migrasi post

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,12 +7,12 @@
         <article class="md-5"
             <h2>{{ $post["judul"] }}</h2>
             <h2>
-                <a href="/posts/{{ $post["slug"] }}">
-                    {{ $post["title"] }}
+                <a href="/posts/{{ $post->id }}">
+                    {{ $post->title }}
                 </a>
             </h2>
             <h5>By: {{ $post["author"]}}</h5>
-            <p>{{ $post["body"] }}</p>    
+            <p>{{ $post->body}}</p>    
 
             
         </article>
