62857e07202bae38d577f778ab32b06ae93fd997
wiwinristanto <wiwinristanto94@gmail.com>
2022-04-18 05:03:21 +0700

    modify posts.blade

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,18 +4,14 @@
 @section('container')
 
     @foreach ($posts as $post)
-        <article class="md-5"
             <h2>{{ $post["judul"] }}</h2>
             <h2>
                 <a href="/posts/{{ $post->slug }}">
                     {{ $post->title }}
                 </a>
             </h2>
-            
+            <p>By. Wiwin R in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>  
             <p>{{ $post->excerpt}}</p>    
-
-            
-        </article>
     @endforeach
 
 @endsection
